aaa3e86124b9dd71232b1a4fb94346367418192c
nekonande <vindelkvist@gmail.com>
2025-03-11 13:58:23 +0100

    Button And Pressure Plate Logic Updated
    Buttons and Pressure plates now have serialized Lists where you add GameObjects instead of several serialized fields
    When a button is pressed or a pressure plate stepped on the button tells the attached GameObjects to Activate() or Deactivate()
    GameObjects that are attached to Buttons and Pressure plates need to use the IActivatable interface and implement Activate() and Deactivate() methods
    Standard interaction range for button changed from 1 to 2
    The cubes that are in the scene have a basic CubeTestScript just to make sure the IActivatable stuff works

--- SoTA - PreProd/Assets/Scripts/ButtonScript.cs
+++ SoTA - PreProd/Assets/Scripts/ButtonScript.cs
@@ -1,28 +1,19 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class ButtonScript : MonoBehaviour, IInteractable
 {
-    [SerializeField] private GameObject objectToMoveDown;
-    [SerializeField] private GameObject objectToMoveUp;
-    [SerializeField] private GameObject objectToMoveLeft;
-    [SerializeField] private GameObject objectToMoveRight;
-    [SerializeField] private float moveDistance = 1f;
-    [SerializeField] private float moveSpeed = 2f;
-    [SerializeField] private float interactionRange = 1f;
+    [SerializeField] private List<GameObject> puzzleElements = new List<GameObject>();
+    [SerializeField] private float interactionRange = 2f;
 
-    private Vector3 initialPositionDown, targetPositionDown;
-    private Vector3 initialPositionUp, targetPositionUp;
-    private Vector3 initialPositionLeft, targetPositionLeft;
-    private Vector3 initialPositionRight, targetPositionRight;
-
-    private bool hasMoved = false;
+    private bool isActive = false;
     private Transform player;
 
     public void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        SetObjectPositions();
     }
 
     void Update()
@@ -33,62 +24,26 @@ public class ButtonScript : MonoBehaviour, IInteractable
         }
     }
 
-    private void SetObjectPositions()
-    {
-        if (objectToMoveDown != null)
-        {
-            initialPositionDown = objectToMoveDown.transform.position;
-            targetPositionDown = initialPositionDown + Vector3.down * moveDistance;
-        }
-
-        if (objectToMoveUp != null)
-        {
-            initialPositionUp = objectToMoveUp.transform.position;
-            targetPositionUp = initialPositionUp + Vector3.up * moveDistance;
-        }
-
-        if (objectToMoveLeft != null)
-        {
-            initialPositionLeft = objectToMoveLeft.transform.position;
-            targetPositionLeft = initialPositionLeft + Vector3.left * moveDistance;
-        }
-
-        if (objectToMoveRight != null)
-        {
-            initialPositionRight = objectToMoveRight.transform.position;
-            targetPositionRight = initialPositionRight + Vector3.right * moveDistance;
-        }
-    }
-
     public void Interact()
     {
-        if (!hasMoved)
+        foreach (GameObject puzzleElement in puzzleElements)
         {
-            if (objectToMoveDown != null) StartCoroutine(MoveObject(objectToMoveDown, targetPositionDown));
-            if (objectToMoveUp != null) StartCoroutine(MoveObject(objectToMoveUp, targetPositionUp));
-            if (objectToMoveLeft != null) StartCoroutine(MoveObject(objectToMoveLeft, targetPositionLeft));
-            if (objectToMoveRight != null) StartCoroutine(MoveObject(objectToMoveRight, targetPositionRight));
-        }
-        else
-        {
-            if (objectToMoveDown != null) StartCoroutine(MoveObject(objectToMoveDown, initialPositionDown));
-            if (objectToMoveUp != null) StartCoroutine(MoveObject(objectToMoveUp, initialPositionUp));
-            if (objectToMoveLeft != null) StartCoroutine(MoveObject(objectToMoveLeft, initialPositionLeft));
-            if (objectToMoveRight != null) StartCoroutine(MoveObject(objectToMoveRight, initialPositionRight));
+            IActivatable activatable = puzzleElement.GetComponent<IActivatable>();
+            if (activatable != null)
+            {
+                if (isActive)
+                {
+                    activatable.Deactivate();
+                }
+                else
+                {
+                    activatable.Activate();
+                }
+            }
         }
 
-        hasMoved = !hasMoved;
+        isActive = !isActive;
     }
-
-    IEnumerator MoveObject(GameObject obj, Vector3 targetPosition)
-    {
-        while (Vector3.Distance(obj.transform.position, targetPosition) > 0.01f)
-        {
-            obj.transform.position = Vector3.MoveTowards(obj.transform.position, targetPosition, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        obj.transform.position = targetPosition; // Ensure exact final position
-    }  
 
     private bool PlayerIsClose()
     {
