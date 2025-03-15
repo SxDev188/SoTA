812fb6f0d402a5f5b8db3dc7890b1d0f8dd0b77a
rixa65 <142931180+rixa65@users.noreply.github.com>
2025-03-15 14:11:40 +0100

    Trying properties with Linus_IActivateable interface

---
+++ SoTA - PreProd/Assets/Scripts/SaveState/Linus_ChangeCubeTest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linus_ChangeCubeTest : MonoBehaviour, Linus_IActivateable
+{
+    public bool Active { get => Active; set => Active = value; }
+
+    public void Activate()
+    {
+    
+        throw new System.NotImplementedException();
+    }
+
+    public void Deactivate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
