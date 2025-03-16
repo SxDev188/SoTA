4f45bed6681476f35f665ece26dbe706becda1d3
nekonande <vindelkvist@gmail.com>
2025-03-16 15:29:11 +0100

    Timer coroutine added
    Buttons now have a toggle in inspector to set it as a timed button, and you can also set the timer duration there
    Button can not be interacted with if timer is running

--- SoTA - PreProd/Assets/Scripts/ButtonScript.cs
+++ SoTA - PreProd/Assets/Scripts/ButtonScript.cs
@@ -6,8 +6,11 @@ using UnityEngine.WSA;
 public class ButtonScript : MonoBehaviour, IInteractable
 {
     [SerializeField] private List<GameObject> puzzleElements = new List<GameObject>();
+    [SerializeField] private bool hasTimer = false;
+    [SerializeField] private float totalTimerDuration = 3;
 
     private bool isActive = false;
+    private bool isTimerRunning = false;
     private Transform player;
 
     public void Start()
@@ -17,6 +20,11 @@ public class ButtonScript : MonoBehaviour, IInteractable
 
     public void Interact()
     {
+        if (isActive && isTimerRunning)
+        {
+            return; //we busy
+        }
+
         foreach (GameObject puzzleElement in puzzleElements)
         {
             IActivatable activatable = puzzleElement.GetComponent<IActivatable>();
@@ -24,15 +32,35 @@ public class ButtonScript : MonoBehaviour, IInteractable
             {
                 if (isActive)
                 {
-                    activatable.Deactivate();
+                    if (!isTimerRunning)
+                    {
+                        activatable.Deactivate();
+                    }
                 }
                 else
                 {
+                    if (hasTimer)
+                    {
+                        StartCoroutine(DeactivateDelayed(activatable));
+                        isTimerRunning = true;
+                    }
                     activatable.Activate();
                 }
             }
         }
+        ToggleButtonState();
+    }
 
+    private void ToggleButtonState()
+    {
         isActive = !isActive;
+    }
+
+    private IEnumerator DeactivateDelayed(IActivatable activatable)
+    {
+        yield return new WaitForSeconds(totalTimerDuration);
+        activatable.Deactivate();
+        isActive = false;
+        isTimerRunning = false;
     }
 }
