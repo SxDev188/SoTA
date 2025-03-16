189c838333646b2fff909a295c7dbb9c8cd7d9b6
rixa65 <142931180+rixa65@users.noreply.github.com>
2025-03-16 12:43:17 +0100

    Added A basic version of SaveStateManager

--- SoTA - PreProd/Assets/Scripts/SaveState/SaveStateManager.cs
+++ SoTA - PreProd/Assets/Scripts/SaveState/SaveStateManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 //A Singelton since it is a Manager
 public class SaveStateManager : MonoBehaviour
 {
     public static SaveStateManager Instance { get; private set; }
-    [SerializeField] private List<GameObject> buttons = new List<GameObject>();
+    [SerializeField] private List<Linus_ButtonScript> buttons = new List<Linus_ButtonScript>();
+    private List<Linus_ButtonScript> savedStateButtons;
+    private GameObject player;
+    private GameObject savedStatePlayer;
+    //private bool saved = false;
     private void Awake()
     {
         // If there is an instance, and it's not me, delete myself.
@@ -20,25 +25,29 @@ public class SaveStateManager : MonoBehaviour
             Instance = this;
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
-
-
-    public void SaveAction()
-    {
-
-    }
-
     public void Save()
     {
-
+        //saved = true;
+        savedStatePlayer.CloneViaSerialization(player);
+        savedStateButtons = new List<Linus_ButtonScript>(buttons);
     }
     public void Load()
     {
+        if (savedStateButtons is not null)
+        {
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                if (buttons[i].IsActive != savedStateButtons[i].IsActive)
+                {
+                    buttons[i].Interact();
+                }
 
+            }
+            player = savedStatePlayer;
+        }
     }
 }
