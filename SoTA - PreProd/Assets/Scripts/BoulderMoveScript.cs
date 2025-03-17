fd0226d98b359499a3728270206688a0b3d6ab2c
flaggstang <161468379+flaggstang@users.noreply.github.com>
2025-03-17 12:08:29 +0100

    Added tags + boulder script improvement & ready for merge

--- SoTA - PreProd/Assets/Scripts/BoulderMoveScript.cs
+++ SoTA - PreProd/Assets/Scripts/BoulderMoveScript.cs
@@ -1,40 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 
 public class BoulderMoveScript : MonoBehaviour
 {
+    [SerializeField] private float interactionRange = 2f;
+
     private bool isMoving = false;
+    private GameObject player;
 
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && PlayerIsClose())
+        {
+            isMoving = !isMoving;
+        }
     }
 
     private void FixedUpdate()
     {
 
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.collider.tag == "Player")
+        // Snap to floor
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 1f))
         {
-            isMoving = true;
-        }
-        else
-        {
-            isMoving = false;
+            if (!isMoving)
+            {
+                transform.position = new Vector3(hit.transform.position.x, transform.position.y, hit.transform.position.z);
+            }
         }
 
         if (isMoving)
         {
-            this.transform.SetParent(collision.transform);
-            Physics.IgnoreCollision(this.GetComponent<Collider>(), collision.collider);
+            this.transform.SetParent(player.transform);
         }
-
+        else
+        {
+            this.transform.parent = null;
+        }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Level Object")
+        {
+            isMoving = false;
+        }
+    }
+
+    private bool PlayerIsClose()
+    {
+        return Vector3.Distance(transform.position, player.transform.position) <= interactionRange;
+    }
 
 }
