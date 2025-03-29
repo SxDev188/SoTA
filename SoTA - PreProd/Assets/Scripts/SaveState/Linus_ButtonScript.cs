3b2d074f5049cb2446d91d86616544f612acc48b
rixa65 <142931180+rixa65@users.noreply.github.com>
2025-03-29 12:13:18 +0100

    New Version of button that works with save state

--- SoTA - PreProd/Assets/Scripts/SaveState/Linus_ButtonScript.cs
+++ SoTA - PreProd/Assets/Scripts/SaveState/Linus_ButtonScript.cs
@@ -1,50 +1,145 @@
+using FMOD.Studio;
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.WSA;
 
-public class Linus_ButtonScript : Linus_Signaler, IInteractable
+public class Linus_ButtonScript : MonoBehaviour, IInteractable
 {
+    [SerializeField] private List<GameObject> puzzleElements = new List<GameObject>();
     [SerializeField] private bool hasTimer = false;
     [SerializeField] private float totalTimerDuration = 3;
 
+    private bool isPushed = false;
     private bool isTimerRunning = false;
-    void Start()
+    private Transform player;
+
+    private EventInstance buttonSFX;
+    public bool IsActive { get { return isPushed; } }
+
+    public void Start()
     {
-        signalerSFX = AudioManager.Instance.CreateInstance(FMODEvents.Instance.ButtonSFX);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        buttonSFX = AudioManager.Instance.CreateInstance(FMODEvents.Instance.ButtonSFX);
+
     }
 
     public void Interact()
     {
         if (isPushed && isTimerRunning)
         {
-            signalerSFX.setParameterByNameWithLabel("ButtonPushState", "PushFail");
-            signalerSFX.start();
+            buttonSFX.setParameterByNameWithLabel("ButtonPushState", "PushFail");
+            buttonSFX.start();
 
             return; //we busy
         }
 
         if (!isPushed && !hasTimer)
         {
-            signalerSFX.setParameterByNameWithLabel("ButtonPushState", "PushDown");
+            buttonSFX.setParameterByNameWithLabel("ButtonPushState", "PushDown");
 
             ActivateAllPuzzleElements();
             isPushed = true;
         }
         else if (!isPushed && hasTimer)
         {
-            signalerSFX.setParameterByNameWithLabel("ButtonPushState", "PushDown");
+            buttonSFX.setParameterByNameWithLabel("ButtonPushState", "PushDown");
 
             StartTimerForAllPuzzleElements();
             isPushed = true;
         }
         else if (isPushed && !isTimerRunning)
         {
-            signalerSFX.setParameterByNameWithLabel("ButtonPushState", "PushUp");
+            buttonSFX.setParameterByNameWithLabel("ButtonPushState", "PushUp");
 
             DeactivateAllPuzzleElements();
             isPushed = false;
         }
 
-        signalerSFX.start();
+        buttonSFX.start();
+    }
+
+    private void ActivateAllPuzzleElements()
+    {
+        foreach (GameObject puzzleElement in puzzleElements)
+        {
+            IActivatable activatable = puzzleElement.GetComponent<IActivatable>();
+
+            if (activatable == null)
+            {
+                continue;
+            }
+
+            activatable.Activate();
+        }
+    }
+    private void DeactivateAllPuzzleElements()
+    {
+        foreach (GameObject puzzleElement in puzzleElements)
+        {
+            IActivatable activatable = puzzleElement.GetComponent<IActivatable>();
+
+            if (activatable == null)
+            {
+                continue;
+            }
+
+            activatable.Deactivate();
+        }
+    }
+
+    private void StartTimerForAllPuzzleElements()
+    {
+        foreach (GameObject puzzleElement in puzzleElements)
+        {
+            IActivatable activatable = puzzleElement.GetComponent<IActivatable>();
+
+            if (activatable == null)
+            {
+                continue;
+            }
+
+            activatable.Activate();
+            StartCoroutine(DeactivateDelayed(activatable));
+            isTimerRunning = true;
+        }
+    }
+
+    private IEnumerator DeactivateDelayed(IActivatable activatable)
+    {
+        yield return new WaitForSeconds(totalTimerDuration);
+        if (isTimerRunning)
+        {
+            activatable.Deactivate();
+            isPushed = false;
+            isTimerRunning = false;
+
+            buttonSFX.setParameterByNameWithLabel("ButtonPushState", "PushUp");
+            buttonSFX.start();
+        }
+    }
+    public void SetState(bool Active)
+    {
+        if (hasTimer)
+        {
+            isTimerRunning = false;
+            isPushed = false;
+            DeactivateAllPuzzleElements();
+            return;
+        }
+        isPushed = Active;
+        if (Active)
+        {
+            ActivateAllPuzzleElements();
+        }
+        else
+        {
+            DeactivateAllPuzzleElements();
+        }
+    }
+    private void ToggleButtonState()
+    {
+        isPushed = !isPushed;
     }
 }
