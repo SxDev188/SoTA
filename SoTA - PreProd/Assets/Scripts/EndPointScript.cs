260b48c2c44d2a0f8e867ec5c402ac06fc755b3d
nekonande <vindelkvist@gmail.com>
2025-04-02 14:30:19 +0200

    Start/End Point Teleports
    Created a new prefab called "Start Point" which is basically just a transform marker
    
    Changed EndPoint script so it takes reference and "connects" to another transform (Start Point), and if player walks onto EndPoint it transfers to the connected StartPoint. EndPoint can also be set to "isExit", in which case the application closes (only works in Unity editor). Each EndPoint also takes a cameraPanDirection which has to be manually set to let the camera know which way to pan.

--- SoTA - PreProd/Assets/Scripts/EndPointScript.cs
+++ SoTA - PreProd/Assets/Scripts/EndPointScript.cs
@@ -1,22 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class EndPointScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Transform nextSpawnPoint;
+    [SerializeField] Vector2 cameraPanDirection;
+    [SerializeField] bool isExit;
+    PlayerController playerController;
+
     void Start()
     {
+        playerController = FindObjectOfType<PlayerController>();
 
+        if (playerController == null)
+        {
+            Debug.LogWarning("PlayerController not found in the scene!");
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("game ended");
-            Application.Quit();            
+            if (isExit)
+            {
+                Debug.LogWarning("Exiting game!");
+                #if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+                #endif
+            }
+
+            if (nextSpawnPoint != null)
+            {
+                playerController.transform.position = nextSpawnPoint.position;
+                CameraPanScript.Instance.PanCamera(cameraPanDirection);
+            }
+            else
+            {
+                Debug.LogWarning("Next spawn point not assigned!");
+            }
         }
     }
 }
