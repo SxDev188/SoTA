bea2a84dc20bf3dadc8289923b8033894a5d39cc
SxDev188 <161468379+SxDev188@users.noreply.github.com>
2025-04-11 18:01:28 +0200

    Scripting stuff

--- SoTA - PreProd/Assets/Scripts/UIScript.cs
+++ SoTA - PreProd/Assets/Scripts/UIScript.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // TWEAKABLE VARIABLES
+    [SerializeField] GameObject pauseMenuObject;
+    [SerializeField] GameObject pauseMenuStartObject;
+    [SerializeField] GameObject menuList;
+
+    // STORING/VALUE VARIABLES
+    private bool isPaused;
+    private GameObject playerObject;
+
+    // ENGINE METHODS ====================================== // 
+    private void Start()
     {
-        
+        playerObject = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnPauseGame(InputValue value) // Add checks for main menu & end screen! Maybe scene name or id?
     {
-        
+        if (isPaused) UnPauseGame();
+        else PauseGame();
     }
+
+    // METHODS ====================================== //
+    public void QuitGame() // Check if we already have a function for this...
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void UnPauseGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        playerObject.GetComponent<PlayerInput>().enabled = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenuObject.SetActive(false);
+        ResetPauseUI();
+    }
+
+    private void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        playerObject.GetComponent<PlayerInput>().enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        pauseMenuObject.SetActive(true);
+    }
+
+    private void ResetPauseUI()
+    {
+        for (int i = 0; i < menuList.transform.childCount; i++)
+        {
+            menuList.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        pauseMenuStartObject.SetActive(true);
+    }
+
 }
