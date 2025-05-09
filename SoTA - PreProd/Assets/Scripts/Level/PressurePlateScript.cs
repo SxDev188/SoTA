caa57a0be83e10cf4084e95b2184c9e2ca8d8d5c
nekonande <vindelkvist@gmail.com>
2025-05-09 12:31:11 +0200

    PressurePlateParticles
    PressurePlates now also have a particle system that playes when activated and deactivated

--- SoTA - PreProd/Assets/Scripts/Level/PressurePlateScript.cs
+++ SoTA - PreProd/Assets/Scripts/Level/PressurePlateScript.cs
@@ -1,4 +1,5 @@
 using FMOD.Studio;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,12 +12,16 @@ public class PressurePlateScript : MonoBehaviour
     private bool isPushedDown = false;
     private Vector3 originalPosition;
 
+    private ParticleSystem pressurePlateParticles;
+
     private EventInstance pressurePlateSFX;
 
 
     private void Start()
     {
         originalPosition = transform.position;
+        pressurePlateParticles = GetComponentInChildren<ParticleSystem>();
+        FindParticleColor();
         pressurePlateSFX = AudioManager.Instance.CreateInstance(FMODEvents.Instance.PressurePlateSFX);
     }
 
@@ -70,13 +75,47 @@ public class PressurePlateScript : MonoBehaviour
             {
                 if (isPushedDown)
                 {
+                    StartCoroutine(PlayAndStopParticleBurst());
                     activatable.Activate();
                 }
                 else
                 {
+                    StartCoroutine(PlayAndStopParticleBurst());
                     activatable.Deactivate();
                 }
             }
+        }
+    }
+
+    IEnumerator PlayAndStopParticleBurst()
+    {
+        pressurePlateParticles.Play();
+        yield return new WaitForSeconds(0.1f); // wait for particles to spawn
+        pressurePlateParticles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+    }
+
+    private void FindParticleColor()
+    {
+        Transform pressurePlate = transform.Find("PressurePlate");
+        Transform pressurePlateParticlesTransform = transform.Find("PressurePlateParticles");
+
+        if (pressurePlate != null && pressurePlateParticlesTransform != null)
+        {
+            MeshRenderer sourceRenderer = pressurePlate.GetComponent<MeshRenderer>();
+            ParticleSystemRenderer psRenderer = pressurePlateParticlesTransform.GetComponent<ParticleSystemRenderer>();
+
+            if (sourceRenderer != null && psRenderer != null)
+            {
+                psRenderer.material = sourceRenderer.sharedMaterial;
+            }
+            else
+            {
+                Debug.LogWarning("MeshRenderer or ParticleSystemRenderer not found.");
+            }
+        }
+        else
+        {
+            Debug.LogError("pressurePlate or PressurePlateParticles not found in the hierarchy.");
         }
     }
 }
