89ebb8c5f8be85d5592b20f64b95c51ed84588c8
rixa65 <142931180+rixa65@users.noreply.github.com>
2025-05-16 10:02:50 +0200

    Boulder Rework Started

--- SoTA - PreProd/Assets/Scripts/Level/Boulder/BoulderSideHitbox.cs
+++ SoTA - PreProd/Assets/Scripts/Level/Boulder/BoulderSideHitbox.cs
@@ -4,11 +4,34 @@ public class BoulderSideHitbox : MonoBehaviour
 {
     [SerializeField] private Vector3 pushDirection;
 
+    private bool blocked = false;
+    private int amountInWay = 0;
+    public bool Blocked { get { return blocked; } }
     public bool WasHitByStar { get; private set; }
     public Vector3 PushDirection { get { return pushDirection; } }
 
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(!other.CompareTag("Star") && !other.CompareTag("Player") && !other.CompareTag("PressurePlate") && !other.CompareTag("EndPoint"))
+        {
+            amountInWay--;
+            if (amountInWay == 0)
+            {
+                blocked = false;
+                Debug.Log(-pushDirection + " is Unblocked");
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
+        if (!other.CompareTag("Star") && !other.CompareTag("Player") && !other.CompareTag("PressurePlate") && !other.CompareTag("EndPoint"))
+        {
+            amountInWay++;
+            blocked = true;
+            Debug.Log(-pushDirection + " is Blocked by " + other.name);
+        }
+
         if (other.gameObject.CompareTag("Star"))
         {
             StarActions starActions = other.gameObject.GetComponent<StarActions>();
@@ -26,6 +49,25 @@ public class BoulderSideHitbox : MonoBehaviour
         }
 
     }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Star"))
+    //    {
+    //        StarActions starActions = other.gameObject.GetComponent<StarActions>();
+
+    //        if (starActions.IsOnPlayer)
+    //        {
+    //            return;
+    //        }
+    //        else if (starActions.IsTraveling)
+    //        {
+    //            starActions.StopTravelToDestination(true);
+    //            WasHitByStar = true;
+    //            this.GetComponentInParent<BoulderStarPushScript>().CheckSideHitboxes();
+    //        }
+    //    }
+
+    //}
 
     public void Reset()
     {
